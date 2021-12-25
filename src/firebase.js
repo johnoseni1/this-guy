f41e62048c3c905be298bc11e9d42f277324f82a
John Oseni <johnoseni26@gmail.com>
2021-12-25 10:58:07 +0100

    doe

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyD0flMB5yk3HZYjXMQIGL25IUn92VZ4gH8',
-  authDomain: 'meet-clone-d6071.firebaseapp.com',
-  projectId: 'meet-clone-d6071',
-  storageBucket: 'meet-clone-d6071.appspot.com',
-  messagingSenderId: '449972301617',
-  appId: '1:449972301617:web:e46fd0f0c5b10109a40a10',
-  measurementId: 'G-GRD7KVE0YH',
+  const firebaseConfig = {
+    apiKey: "AIzaSyD5gGkYAwND26y8nbLmFPPTyneADDx9KXI",
+    authDomain: "test-form-c63aa.firebaseapp.com",
+    databaseURL: "https://test-form-c63aa-default-rtdb.firebaseio.com",
+    projectId: "test-form-c63aa",
+    storageBucket: "test-form-c63aa.appspot.com",
+    messagingSenderId: "726651045197",
+    appId: "1:726651045197:web:5192c8e8db672e8e422304"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
